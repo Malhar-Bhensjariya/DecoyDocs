598d4a9acd32fb466710eff2a9aac5b5176bf739
Malhar-Bhensjariya <mcb110104@gmail.com>
2025-10-31 20:06:31 +0530

    Create test.py

---
+++ tests/test.py
@@ -0,0 +1,3 @@
+from embedder.metadata import read_docx_custom_property
+print(read_docx_custom_property("out/Corporate_Strategy_and_Growth_Report_-_Q1_2026_e19b1de3_embedded.docx", "HoneyUUID"))
+print(read_docx_custom_property("out/Corporate_Strategy_and_Growth_Report_-_Q1_2026_e19b1de3_embedded.docx", "BeaconURL"))
